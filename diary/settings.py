97d32a732264d445db1176318ffea6224a07a37d
Adil Shabab <adilshaba@gmail.com>
2022-07-21 09:28:56 -0700

    error

--- diary/settings.py
+++ diary/settings.py
@@ -138,4 +138,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 
-CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    # "https://example.com",
+    # "https://sub.example.com",
+    # "http://localhost:8080",
+    # "http://127.0.0.1:9000",
+    "https://adil-shabab.github.io/diary-app-frontend/",
+    "http://localhost:3000/",
+]
